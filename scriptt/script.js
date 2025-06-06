b00ec823ba10bbb0ee2e920d1a137ec3e25401ca
testa <oliveirakawan039@gmail.com>
2025-06-06 18:29:50 -0300

    menuhambuguer

---
+++ scriptt/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  const subtitle = document.querySelector(".subtitle");
+
+  nav.classList.toggle("active");
+  subtitle.classList.toggle("oculta");
+}
