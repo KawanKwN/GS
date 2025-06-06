3ea9cbdc44ca24277663fb1eb5f6c7b97b3ef451
testa <oliveirakawan039@gmail.com>
2025-06-06 18:47:40 -0300

    remoçaodecode

--- scriptt/script.js
+++ scriptt/script.js
@@ -1,7 +1,14 @@
 function toggleMenu() {
-  const nav = document.getElementById("nav");
-  const subtitle = document.querySelector(".subtitle");
+    const nav = document.getElementById("nav");
+    const main = document.querySelector("main");
 
-  nav.classList.toggle("active");
-  subtitle.classList.toggle("oculta");
+    nav.classList.toggle("active");
+    main.classList.toggle("inactive");
+  }
+function toogleMenu(){
+    const nav = document.getElementById("nav");
+    const subtitle = document.querySelector(".subtitle");
+
+    nav.classList.toggle("active");
+    subtitle.classList.toggle("oculta")
 }
