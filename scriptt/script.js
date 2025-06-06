a51f0d6a44189b6847d48daa770044964d4f015e
testa <oliveirakawan039@gmail.com>
2025-06-06 19:09:41 -0300

    3-questoesquiz

--- scriptt/script.js
+++ scriptt/script.js
@@ -12,3 +12,20 @@ function toogleMenu(){
     nav.classList.toggle("active");
     subtitle.classList.toggle("oculta")
 }
+ const quizData = [
+    {
+      pergunta: "1. Qual é a principal causa de enchentes nas cidades brasileiras?",
+      opcoes: ["A) Falta de chuvas constantes", "B) Queimadas em áreas urbanas", "C) Acúmulo de lixo e urbanização desordenada", "D) Presença de vegetação nativa"],
+      correta: 2
+    },
+    {
+      pergunta: "2. O que é um sistema de alerta precoce?",
+      opcoes: ["A) Um tipo de sirene de rua", "B) Um protocolo de evacuação manual", "C) Um conjunto de sensores que avisa sobre riscos antes que ocorram", "D) Um aplicativo de previsão do tempo genérico"],
+      correta: 2
+    },
+    {
+      pergunta: "3. Qual dessas tecnologias a HydroWacht utiliza para prevenir enchentes?",
+      opcoes: ["A) Impressoras 3D", "B) Blockchain", "C) Sensores IoT e Inteligência Artificial", "D) Realidade Virtual"],
+      correta: 2
+    },
+    ];
